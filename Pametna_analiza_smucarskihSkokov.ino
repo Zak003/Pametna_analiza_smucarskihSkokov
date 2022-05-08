9f689587345f3a65a546117f1aecea78b7662520
ZakLubej <59819711+Zak003@users.noreply.github.com>
2022-05-08 11:34:08 +0200

    koncano

--- Pametna_analiza_smucarskihSkokov.ino
+++ Pametna_analiza_smucarskihSkokov.ino
@@ -18,6 +18,11 @@ int16_t gx, gy, gz;
 
 bool button = false;
 
+//bool buttonOff = false;
+
+bool isrunning = false;
+
+
 //SD card modul
 const int chipSelect = D8;
 File dataFile;
@@ -31,8 +36,11 @@ char fileName[13];
 //spremenljivka za stevec lajnov
 int line_counter = 0;
 
+int stevec = 0;
+
 void setup() {
     pinMode(16, INPUT);
+    pinMode(0, INPUT);
     pinMode(LED_BUILTIN, OUTPUT);
       
       #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -45,35 +53,44 @@ void setup() {
 
       accelgyro.initialize();
 
-      //kalibracija
-      accelgyro.setXGyroOffset(220);
-      accelgyro.setYGyroOffset(76);
-      accelgyro.setZGyroOffset(-85);
-      accelgyro.setZAccelOffset(1788); 
-  
-      // make sure it worked (returns 0 if so)
-      if (devStatus == 0) {
-          // Calibration Time: generate offsets and calibrate our MPU6050
-          accelgyro.CalibrateAccel(6);
-          accelgyro.CalibrateGyro(6);
-          accelgyro.PrintActiveOffsets();
-      }
+        //kalibracija
+        accelgyro.setXGyroOffset(220);
+        accelgyro.setYGyroOffset(76);
+        accelgyro.setZGyroOffset(-85);
+        accelgyro.setZAccelOffset(1788); 
+    
+        // make sure it worked (returns 0 if so)
+        if (devStatus == 0) {
+            // Calibration Time: generate offsets and calibrate our MPU6050
+            accelgyro.CalibrateAccel(6);
+            accelgyro.CalibrateGyro(6);
+            accelgyro.PrintActiveOffsets();
+        }
+
+      
 
       if (!SD.begin(chipSelect)) {
         Serial.println("SD card initialization failed!");
         while (1);
       }
 
-      //za pregled in ustvarjanje txt fajlov 
-      for (int n = 0; n < LIMIT; n++) {
-        sprintf(fileName, "file%.3d.txt", n);
-        if (SD.exists(fileName)) continue;
-        dataFile = SD.open(fileName, FILE_WRITE);
-        break; 
-      }      
+          
   }
   
   void loop() {
+
+      if(isrunning == false){
+        //za pregled in ustvarjanje txt fajlov 
+        for (int n = 0; n < LIMIT; n++) {
+          sprintf(fileName, "file%.3d.txt", n);
+          if (SD.exists(fileName)) continue;
+          dataFile = SD.open(fileName, FILE_WRITE);
+          break; 
+        }
+        isrunning = true;  
+      }
+
+      
       accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
       
       #ifdef OUTPUT_READABLE_ACCELGYRO
@@ -82,7 +99,16 @@ void setup() {
       if (digitalRead(16) == HIGH){
         button = !button;
         delay(200);
+        stevec++;
+        Serial.print(stevec);
       }
+
+      if(stevec == 2){
+          isrunning = false;
+          stevec = 0;
+          line_counter = 0;
+          Serial.print("Nov skok");
+        }
 
       dataFile = SD.open(fileName, FILE_WRITE);
 
@@ -93,7 +119,7 @@ void setup() {
         Serial.print("az:");Serial.print(az); Serial.print(";");
         Serial.print("gx:");Serial.print(gx); Serial.print(";");
         Serial.print("gy:");Serial.print(gy); Serial.print(";");
-        Serial.print("gz:");Serial.print(gz); Serial.println(";");
+        Serial.print("gz:");Serial.print(gz); Serial.println("");
         
         //zapis podatkov na sd card 
         if (dataFile) {
@@ -102,7 +128,7 @@ void setup() {
           dataFile.print(ay);dataFile.print(";");
           dataFile.print(az);dataFile.print(";");
           dataFile.print(gx);dataFile.print(";");
-          dataFile.print(gy);dataFile.print(";");dataFile.println("");
+          dataFile.print(gy);dataFile.print("");dataFile.println("");
           dataFile.close();
           //Serial.println("Writing to SD card was DONE!");
          }
